eb84c9b257c8964084937fd0b1d9b3fd09fa3921
zyc <1343786337@qq.com>
2019-05-20 23:35:46 +0800

    添加feign配置

---
+++ core-service/src/main/java/com/zyc/core/feign/FeignHeaderConfig.java
@@ -0,0 +1,48 @@
+package com.zyc.core.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * feign请求配置
+ *
+ * @author zyc
+ */
+@Configuration
+public class FeignHeaderConfig implements RequestInterceptor {
+
+    /**
+     * 转发feign请求头
+     *
+     * @param template
+     */
+    @Override
+    public void apply(RequestTemplate template) {
+        // 当Hystrix熔断策略为THREAD时，requestAttributes为null
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return;
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                Enumeration<String> values = request.getHeaders(name);
+                while (values.hasMoreElements()) {
+                    String value = values.nextElement();
+                    template.header(name, value);
+                }
+            }
+        }
+
+    }
+
+}
