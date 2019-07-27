e19a631127d3eba0aa6f48a47e5682944370c87f
zyc <1343786337@qq.com>
2019-07-27 23:43:01 +0800

    code review

--- core-service/src/main/java/com/zyc/core/util/GsonUtil.java
+++ core-service/src/main/java/com/zyc/core/util/GsonUtil.java
@@ -2,10 +2,6 @@ package com.zyc.core.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-import java.lang.reflect.Type;
 
 /**
  * Gson工具类
@@ -20,15 +16,4 @@ public class GsonUtil {
         return GSON.toJson(src);
     }
 
-    public static String toJson(Object src, Type typeOfSrc) {
-        return GSON.toJson(src, typeOfSrc);
-    }
-
-    public static JsonElement toJsonElement(Object src) {
-        return GSON.toJsonTree(src);
-    }
-
-    public static JsonElement toJsonElement(String src) {
-        return new JsonParser().parse(src);
-    }
 }
