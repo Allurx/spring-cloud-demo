65ac75083ea053ad44923d55ea11473a6ec83dfc
zyc <1343786337@qq.com>
2019-07-30 22:54:03 +0800

    code review

--- core-service/src/main/java/com/zyc/core/constant/GlobalConstant.java
+++ core-service/src/main/java/com/zyc/core/constant/GlobalConstant.java
@@ -5,8 +5,6 @@ package com.zyc.core.constant;
  */
 public class GlobalConstant {
 
-    public static final String KEY = "7ae0da0f-564b-4d58-9b28-607d97f67696";
-
     public static final String SWAGGER_PATH_REGEX = "";
 
     public static final String BASE_PACKAGE = "com.zyc";
